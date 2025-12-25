ca6c96db9ea4d2a4df4fc483a59bfca579ddce8c
Alistair Smith <hi@alistair.sh>
2025-12-25 08:26:32 +0000

    Test 8 variants

--- repro_minimal.v
+++ repro_minimal.v
@@ -5,27 +5,35 @@ module main
 pub struct Span { pub: line int col int }
 fn span() Span { return Span{} }
 
-// === AST (4 variants) ===
+// === AST (8 variants) ===
 pub struct AstNumber { pub: value string span Span }
 pub struct AstString { pub: value string span Span }
 pub struct AstIdent { pub: name string span Span }
 pub struct AstError { pub: msg string span Span }
-pub type AstExpr = AstNumber | AstString | AstIdent | AstError
+pub struct AstBool { pub: value bool span Span }
+pub struct AstArray { pub: elems []AstExpr span Span }
+pub struct AstBinary { pub: left AstExpr right AstExpr span Span }
+pub struct AstBlock { pub: items []AstExpr span Span }
+pub type AstExpr = AstNumber | AstString | AstIdent | AstError | AstBool | AstArray | AstBinary | AstBlock
 
 pub struct AstVarBinding { pub: name AstIdent init AstExpr span Span }
 pub struct AstFnDecl { pub: name AstIdent body AstExpr span Span }
 pub struct AstExport { pub: decl AstStmt span Span }  // recursive!
 pub type AstStmt = AstVarBinding | AstFnDecl | AstExport
 
-pub struct AstBlock { pub: body []AstNode span Span }
+pub struct AstProgram { pub: body []AstNode span Span }
 pub type AstNode = AstStmt | AstExpr
 
-// === TYPED AST (4 variants) ===
+// === TYPED AST (8 variants) ===
 pub struct TNumber { pub: value string span Span }
 pub struct TString { pub: value string span Span }
 pub struct TIdent { pub: name string span Span }
 pub struct TError { pub: msg string span Span }
-pub type TExpr = TNumber | TString | TIdent | TError
+pub struct TBool { pub: value bool span Span }
+pub struct TArray { pub: elems []TExpr span Span }
+pub struct TBinary { pub: left TExpr right TExpr span Span }
+pub struct TBlock { pub: items []TExpr span Span }
+pub type TExpr = TNumber | TString | TIdent | TError | TBool | TArray | TBinary | TBlock
 
 pub struct TVarBinding { pub: name TIdent init TExpr span Span }
 pub struct TFnDecl { pub: name TIdent body TExpr span Span }
@@ -33,7 +41,7 @@ pub struct TExport { pub: decl TStmt span Span }
 pub type TStmt = TVarBinding | TFnDecl | TExport
 
 pub struct TBlockItem { pub: is_stmt bool stmt TStmt expr TExpr }
-pub struct TBlock { pub: body []TBlockItem span Span }
+pub struct TProgram { pub: body []TBlockItem span Span }
 
 // === Type Environment with map ===
 pub type Type = TypeNone
@@ -47,20 +55,20 @@ pub fn (mut e TypeEnv) define(name string, t Type) { e.bindings[name] = t }
 // === Type Checker ===
 pub struct TypeChecker { mut: env TypeEnv }
 
-pub fn check(program AstBlock) TBlock {
+pub fn check(program AstProgram) TProgram {
 	mut c := TypeChecker{env: new_env()}
-	return c.check_block(program)
+	return c.check_program(program)
 }
 
-fn (mut c TypeChecker) check_block(block AstBlock) TBlock {
+fn (mut c TypeChecker) check_program(prog AstProgram) TProgram {
 	mut items := []TBlockItem{}
-	for node in block.body {
+	for node in prog.body {
 		match node {
 			AstStmt { items << TBlockItem{is_stmt: true, stmt: c.check_stmt(node)} }
 			AstExpr { items << TBlockItem{is_stmt: false, expr: c.check_expr(node)} }
 		}
 	}
-	return TBlock{body: items, span: block.span}
+	return TProgram{body: items, span: prog.span}
 }
 
 fn (mut c TypeChecker) check_stmt(stmt AstStmt) TStmt {
@@ -85,12 +93,15 @@ fn (mut c TypeChecker) check_expr(expr AstExpr) TExpr {
 		AstString { return TString{value: expr.value, span: expr.span} }
 		AstIdent { return TIdent{name: expr.name, span: expr.span} }
 		AstError { return TError{msg: expr.msg, span: expr.span} }
+		AstBool { return TBool{value: expr.value, span: expr.span} }
+		AstArray { return TArray{span: expr.span} }
+		AstBinary { return TBinary{left: c.check_expr(expr.left), right: c.check_expr(expr.right), span: expr.span} }
+		AstBlock { return TBlock{span: expr.span} }
 	}
 }
 
 fn main() {
-	// Create some test AST nodes
-	program := AstBlock{
+	program := AstProgram{
 		body: [
 			AstNode(AstStmt(AstVarBinding{
 				name: AstIdent{name: 'x', span: span()}
